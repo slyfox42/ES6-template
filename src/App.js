998c6f55e82c59430c6f0dd1185a949f9795d5f8
sokafootball <laurent.capello@googlemail.com>
2019-01-16 18:22:04 +0100

    Button Component (#2)
    * removed .env file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss'
+import Button from './components/atoms/Button'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="body-container">
+      <div className="title">Hello world</div>
+      <Button />
+    </div>
+  )
+}
+
+export default App
